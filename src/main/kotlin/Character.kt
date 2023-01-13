63ad6df074026b43aa1d4d009b7024627d9503bc
Pnter <chanmansang30@gmail.com>
2023-01-13 09:37:34 +0800

    lab1

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,2 +1,20 @@
-class Character {
+abstract class Character(val name: String,val health: Int) {
+   // abstract var health: Int      // Abstract Property
+   // abstract var name: String
+    abstract fun drawingPhase()
+    abstract fun discardPhase()  // Abstract Methods
+}
+class characters(name: String,health: Int) : Character(name,health) {
+
+    override fun drawingPhase() {
+        println("$name turn:")
+        println("$name turn:")
+    }
+    override fun discardPhase() {
+        println()
+    }
+}
+fun main(args: Array<String>) {
+    val eng = characters("Kwan Yue",4)
+
 }
